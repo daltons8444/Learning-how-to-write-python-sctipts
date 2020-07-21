6fcebf3837bd7327b0233dfaab34e5654f4783a0
daltons8444 <daltonsartin54@gmail.com>
2020-07-21 14:44:11 -0500

    Add files via upload

---
+++ Setup_test.py
@@ -0,0 +1,11 @@
+a = 'Dalton'
+b = 'Tanner'
+print('Welcome, Who are you?')
+a = input()
+b = input()
+a1 = 'DPassword'
+b1 = 'TPassword'
+if a == 'Dalton' or b == 'Tanner':
+    print ('What is your password')
+testing if this updates
+
